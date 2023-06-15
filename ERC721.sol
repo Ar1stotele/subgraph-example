57dbcd36fe654041d1d71f469165b6d9ef9c2bb4
Ar1stotele <20100274@ibsu.edu.ge>
2023-06-15 21:48:24 +0400

    Initialize: Subgraph simple example

---
+++ ERC721.sol
@@ -0,0 +1,42 @@
+//Contract based on [https://docs.openzeppelin.com/contracts/3.x/erc721](https://docs.openzeppelin.com/contracts/3.x/erc721)
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract NoveltyNFT is ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    mapping(address => uint256[]) userNFTs;
+
+    constructor() ERC721("NoveltyNFT", "NFT") {}
+
+    event NFTMinted(uint256 indexed tokenId, address indexed owner);
+
+    function mintNFT(address recipient, string memory tokenURI)
+        public
+        onlyOwner
+        returns (uint256)
+    {
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(recipient, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        userNFTs[recipient].push(_tokenIds.current());
+        emit NFTMinted(newItemId, recipient);
+
+        return newItemId;
+    }
+
+    function getUserNFTs(address userAddress)
+        public
+        view
+        returns (uint256[] memory)
+    {
+        return userNFTs[userAddress];
+    }
+}
